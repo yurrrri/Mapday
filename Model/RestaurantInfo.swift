f648b5ca6ac982aa7eca2e9c9c3e9a6187f610aa
yurrrri8403 <leeyuri.andapp@gmail.com>
2025-08-05 19:56:02 +0900

    refactor: 필요한 정보만 받아오기 위해 PlaceInfo property 제거

--- Model/RestaurantInfo.swift
+++ Model/RestaurantInfo.swift
@@ -18,22 +18,18 @@ struct ResturantInfo: Codable {
 
 // MARK: - PlaceInfo
 struct PlaceInfo: Codable {
-    let placeName, distance: String
+    let placeName: String
     let placeURL: String
     let categoryName, addressName, roadAddressName, id: String
-    let phone, categoryGroupCode, categoryGroupName, x: String
-    let y: String
+    let phone, x, y: String
 
     enum CodingKeys: String, CodingKey {
         case placeName = "place_name"
-        case distance
         case placeURL = "place_url"
         case categoryName = "category_name"
         case addressName = "address_name"
         case roadAddressName = "road_address_name"
         case id, phone
-        case categoryGroupCode = "category_group_code"
-        case categoryGroupName = "category_group_name"
         case x, y
     }
 }
---
+++ View/Common/RestaurantInfoListView.swift
@@ -0,0 +1,21 @@
+//
+//  RestaurantInfoListView.swift
+//  MapDay
+//
+//  Created by 이유리 on 8/5/25.
+//
+
+import SwiftUI
+
+struct RestaurantInfoListView: View {
+    
+    let place: PlaceInfo
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantInfoListView(place: PlaceInfo(placeName: "", placeURL: "", categoryName: "", addressName: "", roadAddressName: "", id: "", phone: "", x: "", y: ""))
+}
